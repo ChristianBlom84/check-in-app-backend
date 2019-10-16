7930e4a9ac76ae0b3a4ba384a34dd9af77a69c92
Nils <nils@mw-008.local>
2019-10-16 13:55:43 +0200

    try to build route for push message

--- src/routes/PushNotifications.ts
+++ src/routes/PushNotifications.ts
@@ -1,7 +1,8 @@
-import { Request, Response, Router } from "express";
-import { BAD_REQUEST, CREATED, OK } from "http-status-codes";
-import { paramMissingError, logger, adminMW } from "@shared";
-import { Subscriber } from "../models/subscriber";
+import { Request, Response, Router } from 'express';
+import { BAD_REQUEST, CREATED, OK } from 'http-status-codes';
+import { paramMissingError, logger, adminMW } from '@shared';
+import { Subscriber } from '../models/subscriber';
+import { Expo, ExpoPushMessage, ExpoPushTicket, ExpoPushToken } from './expo';
 
 interface SubscriberData {
   pushToken: string;
@@ -14,22 +15,69 @@ const router = Router();
 /******************************************************************************
  *                Register Expo Push Token - "POST /api/push"
  ******************************************************************************/
-router.post("/", async (req: Request, res: Response) => {
+router.post('/', async (req: Request, res: Response) => {
   try {
     const subscriberData: SubscriberData = req.body;
     console.log(subscriberData);
-    const subscriber = await Subscriber.findOne({ pushToken: subscriberData.pushToken });
+    const subscriber = await Subscriber.findOne({
+      pushToken: subscriberData.pushToken
+    });
     if (!subscriber) {
       Subscriber.create(subscriberData);
       return res.status(CREATED).json(subscriberData);
     }
-    return res.status(BAD_REQUEST).json({ error: "Device already registered." })
+    return res
+      .status(BAD_REQUEST)
+      .json({ error: 'Device already registered.' });
   } catch (err) {
     logger.error(err.message, err);
     return res.status(BAD_REQUEST).json({
       error: err.message
     });
   }
+});
+
+router.post('/send', async (req: Request, res: Response) => {
+  let messages = [];
+  let expo = new Expo();
+
+  const subscribers = await Subscriber.find({}).select('pushToken -_id');
+  console.log(subscribers); //res.send(pushTokens);
+  for (let subscriber of subscribers) {
+    if (!Expo.isExpoPushToken(subscriber.pushToken)) {
+      console.error(
+        `Push token ${subscriber.pushToken} is not a valid Expo push token`
+      );
+      continue;
+    }
+    messages.push({
+      to: subscriber.pushToken,
+      sound: 'default',
+      body: 'This is a test notification',
+      data: { withSome: 'data' }
+    });
+  }
+
+  let chunks = expo.chunkPushNotifications(messages);
+  let tickets = [];
+  (async () => {
+    // Send the chunks to the Expo push notification service. There are
+    // different strategies you could use. A simple one is to send one chunk at a
+    // time, which nicely spreads the load out over time:
+    for (let chunk of chunks) {
+      try {
+        let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+        console.log(ticketChunk);
+        tickets.push(...ticketChunk);
+        // NOTE: If a ticket contains an error code in ticket.details.error, you
+        // must handle it appropriately. The error codes are listed in the Expo
+        // documentation:
+        // https://docs.expo.io/versions/latest/guides/push-notifications#response-format
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  })();
 });
 
 export default router;
